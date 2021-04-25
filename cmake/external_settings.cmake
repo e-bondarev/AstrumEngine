7813624877bae561d5a769a6f2f0df120d3fbc51
azaremde <82723220+e-bondarev@users.noreply.github.com>
2021-04-25 16:50:55 +0200

    Linked and tested glew and glfw

---
+++ cmake/external_settings.cmake
@@ -0,0 +1,5 @@
+set(BUILD_SHARED_LIBS               OFF)
+set(BUILD_UTILS                     OFF)
+set(GLFW_BUILD_DOCS                 OFF)
+set(GLFW_INSTALL                    OFF)
+set(USE_MSVC_RUNTIME_LIBRARY_DLL    OFF)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout << "Hello, world!\n";
+    GLFWwindow* window;
+    
+    glfwInit();
+    glewInit();
+
+    glfwDefaultWindowHints();
+
+    window = glfwCreateWindow(800, 600, "Window", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
 
     return 0;
 }
